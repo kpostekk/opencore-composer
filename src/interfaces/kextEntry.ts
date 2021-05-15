77d3a5c600b6fe4782c50e45208cc135ab24875a
Krystian Postek <krystian@postek.eu>
2021-05-15 22:25:28 +0200

    Create Kext interface
    and nothing more

---
+++ src/interfaces/kextEntry.ts
@@ -0,0 +1,10 @@
+export interface KextEntry {
+  Arch: string
+  BundlePath: string
+  Comment: string
+  Enabled: boolean
+  ExecutablePath: string
+  MaxKernel: string
+  MinKernel: string
+  PlistPath: string
+}
