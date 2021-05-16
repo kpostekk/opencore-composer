984759aba999cfc0a4d2daf8d86212543c8d6d00
Krystian Postek <krystian@postek.eu>
2021-05-16 16:43:55 +0200

    Make dir cleaner more Promising hahaha

--- src/cleanMan.ts
+++ src/cleanMan.ts
@@ -1,5 +1,5 @@
 import Composition from './interfaces/composition'
-import { copySync, readdirSync, removeSync } from 'fs-extra'
+import { copy, readdir, remove } from 'fs-extra'
 
 export default class FileMan {
   private readonly composition: Composition
@@ -8,11 +8,11 @@ export default class FileMan {
     this.composition = composition
   }
 
-  copyMissingACPI (acpiPath: string, assetsPath: string) {
+  async copyMissingACPI (acpiPath: string, assetsPath: string) {
     for (const acpiName of this.composition.acpi?.copy ?? []) {
-      if (!readdirSync(acpiPath).includes(acpiName)) {
+      if (!(await readdir(acpiPath)).includes(acpiName)) {
         try {
-          copySync(assetsPath + acpiName, acpiPath + acpiName)
+          await copy(assetsPath + acpiName, acpiPath + acpiName)
           console.log('Copy ' + acpiName + ' to ACPI directory')
         } catch (err) {
           console.warn(err)
@@ -21,11 +21,11 @@ export default class FileMan {
     }
   }
 
-  copyMissingDrivers (driversPath: string, assetsPath: string) {
+  async copyMissingDrivers (driversPath: string, assetsPath: string) {
     for (const driverName of this.composition.uefi.drivers) {
-      if (!readdirSync(driversPath).includes(driverName)) {
+      if (!(await readdir(driversPath)).includes(driverName)) {
         try {
-          copySync(assetsPath + driverName, driversPath + driverName)
+          await copy(assetsPath + driverName, driversPath + driverName)
           console.log('Copy ' + driverName + ' to Drivers directory')
         } catch (err) {
           console.warn(err)
@@ -34,21 +34,21 @@ export default class FileMan {
     }
   }
 
-  cleanDrivers (driversPath: string) {
-    readdirSync(driversPath).forEach((filePath) => {
+  async cleanDrivers (driversPath: string) {
+    for (const filePath of await readdir(driversPath)) {
       if (!this.composition.uefi.drivers.includes(filePath)) {
-        removeSync(driversPath + filePath)
+        await remove(driversPath + filePath)
         console.log('Removed ' + filePath + ' from Drivers directory')
       }
-    })
+    }
   }
 
-  cleanTools (toolsPath: string) {
-    readdirSync(toolsPath).forEach((filePath) => {
+  async cleanTools (toolsPath: string) {
+    for (const filePath of await readdir(toolsPath)) {
       if (!this.composition.uefi.tools?.includes(filePath) ?? []) {
-        removeSync(toolsPath + filePath)
+        await remove(toolsPath + filePath)
         console.log('Removed ' + filePath + ' from Tools directory')
       }
-    })
+    }
   }
 }
